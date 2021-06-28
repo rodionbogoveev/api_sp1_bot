036bbf351893664d03183a3e5ce4a0d1091bc4fb
rodionbogoveev <rodiononon@yandex.ru>
2021-06-29 00:30:45 +0300

    Не работает pytest и не реализовано логирование

--- homework.py
+++ homework.py
@@ -29,23 +29,28 @@ def get_homeworks(current_timestamp):
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
     homework_statuses = requests.get(url, headers=headers, params=payload).json()
-    homework = homework_statuses['homeworks'][0]
-    return parse_homework_status(homework)
-    global current_timestamp
-    current_timestamp = homework_statuses['current_date']
+    try:
+        homework = homework_statuses['homeworks'][0]
+        timestamp['current'] = homework_statuses['current_date']
+        return parse_homework_status(homework)
+    except IndexError:
+        pass
 
 def send_message(message):
-    return bot.send_message(CHAT_ID, message)
+    if message is not None:
+        return bot.send_message(CHAT_ID, message)
+    pass
 
-current_timestamp = 0
+timestamp = {
+    'current': 0
+}
 
 def main():
 
     while True:
         try:
-            send_message(get_homeworks(current_timestamp))
-            print('работаю')
-            time.sleep(5)
+            send_message(get_homeworks(timestamp['current']))
+            time.sleep(5 * 60)
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
