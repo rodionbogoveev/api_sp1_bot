4267bc57d113b7361a48c402ef9ad681fc9c36fa
rodionbogoveev <rodiononon@yandex.ru>
2021-06-30 00:23:05 +0300

    Deploy to Heroku

--- homework.py
+++ homework.py
@@ -1,11 +1,10 @@
+import logging
 import os
 import time
+
 import requests
 import telegram
 from dotenv import load_dotenv
-import logging
-from logging.handlers import RotatingFileHandler
-
 
 load_dotenv()
 
@@ -17,7 +16,7 @@ bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='program.log', 
+    filename='program.log',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
     filemode='a'
 )
@@ -34,26 +33,27 @@ def parse_homework_status(homework):
         return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
     pass
 
+
 def get_homeworks(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload).json()
+    homework_statuses = requests.get(
+        url, headers=headers, params=payload).json()
     timestamp['current'] = homework_statuses['current_date']
     return homework_statuses
 
+
 def send_message(message):
     if message is not None:
-        try:
-            return bot.send_message(CHAT_ID, message)
-        except Exception as error:
-            logging.error(error, exc_info=True)
-            return bot.send_message(chat_id=CHAT_ID, text=str(error))
+        return bot.send_message(CHAT_ID, message)
     pass
+
 
 timestamp = {
     'current': 0
 }
+
 
 def main():
 
@@ -70,8 +70,10 @@ def main():
             pass
             time.sleep(5)
 
-        except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+        except Exception as error:
+            logging.error(error, exc_info=True)
+            bot.send_message(
+                chat_id=CHAT_ID, text=f'Bot has down by error - {error}')
             time.sleep(5)
 
 
