8225aaa20ba3fbfaaf6e0b8375f1c325c42423dd
Родион Боговеев <rodiononon@yandex.ru>
2021-07-02 16:25:15 +0300

    Исправления после ревью

--- homework.py
+++ homework.py
@@ -27,60 +27,60 @@ class TGBotException(Exception):
 
 
 def parse_homework_status(homework):
-    if 'homework_name' and 'status' not in homework:
+    if 'name' and 'status' not in homework:
         raise TGBotException('The message does not contain required fields')
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-    if homework_status == 'rejected':
+    name = homework['homework_name']
+    status = homework['status']
+    verdict_template = 'У вас проверили работу "{}"!\n\n{}'
+    if status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
-    if homework_status == 'approved':
+        return verdict_template.format(name, verdict)
+    elif status == 'approved':
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return verdict_template.format(name, verdict)
+    elif status == 'reviewing':
+        return 'Работа на проверке.'
+    else:
+        raise TGBotException('Unknown homework status')
 
 
 def get_homeworks(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(
-        url, headers=headers, params=payload).json()
-    
-    send_error_message(error)
-    global timestamp
-    timestamp = homework_statuses['current_date']
-    return homework_statuses
+    try:
+        homework_statuses = requests.get(
+            url, headers=headers, params=payload).json()
+        return homework_statuses
+    except Exception as error:
+        send_error_message(error)
 
 
 def send_message(message):
     if message is not None:
-        return bot.send_message(CHAT_ID, message)
+        bot.send_message(CHAT_ID, message)
         logging.info('Message sent')
-    raise TGBotException('The message does not required')
+    else:
+        raise TGBotException('The message does not required')
 
 
 def send_error_message(error):
     logging.error(error, exc_info=True)
     message = f'Bot has down by error - {error}'
-    return bot.send_message(chat_id=CHAT_ID, text=message)
-
-
-timestamp = 0
+    bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-
     while True:
         try:
+            timestamp = int(time.time())
             logging.debug('Start')
-            print(timestamp)
             homeworks = get_homeworks(timestamp)
             if len(homeworks['homeworks']) != 0:
                 homework = homeworks['homeworks'][0]
                 message = parse_homework_status(homework)
                 send_message(message)
-            pass
             time.sleep(5)
-
         except Exception as error:
             send_error_message(error)
             time.sleep(5)
