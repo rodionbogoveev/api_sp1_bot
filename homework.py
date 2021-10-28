59e8f505684796f8d7b2b7b1b86769ff3c3cd6aa
Родион Боговеев <rodiononon@yandex.ru>
2021-10-28 15:03:20 +0300

    Починил ссылку

--- homework.py
+++ homework.py
@@ -12,7 +12,7 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-URL = 'https://praсtiсum.yandex.ru/api/user_api/homework_statuses/'
+URL = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
